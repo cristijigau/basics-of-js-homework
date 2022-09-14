2d07259c2bafe1e4eee75e07624402673c7f6203
Daniil Birsan <Daniil.Birsan@endava.com>
2022-09-14 12:27:44 +0300

    2-fiz-buzz done

--- 2-fiz-buzz.js
+++ 2-fiz-buzz.js
@@ -13,3 +13,18 @@
  */
 
 // Your code here.
+(function () {
+    for (let i = 1; i <= 100; i++) {
+        let fizz = i % 3
+        let buzz = i % 5
+        if (fizz == 0 && buzz == 0) {
+            console.log("FizzBuzz")
+        } else if (fizz == 0) {
+            console.log("Fizz")
+        } else if (buzz == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+})()
